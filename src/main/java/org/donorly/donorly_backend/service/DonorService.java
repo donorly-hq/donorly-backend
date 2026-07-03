b514ed8a29c389ea1560f580f40d7a46e582e2ae
donorly-hq <sam@donorly.org>
2026-07-03 05:42:04 +0530

    Refactor DonorService methods and annotations

--- src/main/java/org/donorly/donorly_backend/service/DonorService.java
+++ src/main/java/org/donorly/donorly_backend/service/DonorService.java
@@ -1,42 +1,36 @@
 package org.donorly.donorly_backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.Donor;
 import org.donorly.donorly_backend.repository.DonorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class DonorService {
 
-    @Autowired
-    private DonorRepository donorRepository;
+    private final DonorRepository donorRepository;
 
-    public List<Donor> getAllDonors() {
+    public List<Donor> getAll() {
         return donorRepository.findAll();
     }
 
-    public Optional<Donor> getDonorById(String id) {
+    public Optional<Donor> getById(String id) {
         return donorRepository.findById(id);
     }
 
-    public List<Donor> getDonorsByAmbassador(String ambassadorId) {
+    public List<Donor> getByAmbassadorId(String ambassadorId) {
         return donorRepository.findByAmbassadorId(ambassadorId);
     }
 
-    public Donor createDonor(Donor donor) {
-        donor.setCreatedAt(LocalDateTime.now());
+    public Donor save(Donor donor) {
         return donorRepository.save(donor);
     }
 
-    public Donor updateDonor(String id, Donor updated) {
-        updated.setId(id);
-        return donorRepository.save(updated);
-    }
-
-    public void deleteDonor(String id) {
+    public void delete(String id) {
         donorRepository.deleteById(id);
     }
 }
