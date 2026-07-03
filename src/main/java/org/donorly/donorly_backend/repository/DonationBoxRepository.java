c45cb2f5ec11e66a79a5602109548646f503fcdc
donorly-hq <sam@donorly.org>
2026-07-03 05:39:54 +0530

    Refactor DonationBoxRepository to use JPA
    Updated DonationBoxRepository to use JpaRepository and modified method signatures.

--- src/main/java/org/donorly/donorly_backend/repository/DonationBoxRepository.java
+++ src/main/java/org/donorly/donorly_backend/repository/DonationBoxRepository.java
@@ -1,11 +1,9 @@
 package org.donorly.donorly_backend.repository;
 
 import org.donorly.donorly_backend.model.DonationBox;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface DonationBoxRepository extends MongoRepository<DonationBox, String> {
-    List<DonationBox> findByStatus(String status);
-    List<DonationBox> findByVolunteerId(String volunteerId);
-    List<DonationBox> findByArea(String area);
+public interface DonationBoxRepository extends JpaRepository<DonationBox, String> {
+    List<DonationBox> findByAssignedAmbassadorId(String ambassadorId);
 }
