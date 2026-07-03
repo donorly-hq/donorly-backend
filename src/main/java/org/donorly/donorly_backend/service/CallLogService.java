bb5413f7d33c127af519e27545d16c174be0141a
donorly-hq <sam@donorly.org>
2026-07-03 05:43:39 +0530

    Refactor CallLogService methods and annotations

--- src/main/java/org/donorly/donorly_backend/service/CallLogService.java
+++ src/main/java/org/donorly/donorly_backend/service/CallLogService.java
@@ -1,42 +1,40 @@
 package org.donorly.donorly_backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.CallLog;
 import org.donorly.donorly_backend.repository.CallLogRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class CallLogService {
 
-    @Autowired
-    private CallLogRepository callLogRepository;
+    private final CallLogRepository callLogRepository;
 
-    public List<CallLog> getAllCallLogs() {
+    public List<CallLog> getAll() {
         return callLogRepository.findAll();
     }
 
-    public Optional<CallLog> getCallLogById(String id) {
+    public Optional<CallLog> getById(String id) {
         return callLogRepository.findById(id);
     }
 
-    public List<CallLog> getCallLogsByDonor(String donorId) {
+    public List<CallLog> getByDonorId(String donorId) {
         return callLogRepository.findByDonorId(donorId);
     }
 
-    public CallLog createCallLog(CallLog callLog) {
-        callLog.setCalledAt(LocalDateTime.now());
+    public List<CallLog> getByAmbassadorId(String ambassadorId) {
+        return callLogRepository.findByAmbassadorId(ambassadorId);
+    }
+
+    public CallLog save(CallLog callLog) {
         return callLogRepository.save(callLog);
     }
 
-    public CallLog updateCallLog(String id, CallLog updated) {
-        updated.setId(id);
-        return callLogRepository.save(updated);
-    }
-
-    public void deleteCallLog(String id) {
+    public void delete(String id) {
         callLogRepository.deleteById(id);
     }
 }
