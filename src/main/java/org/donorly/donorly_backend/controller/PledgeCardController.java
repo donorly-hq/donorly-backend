106fa9ea21881e9ace8b7a235cde2034dce4cf71
donorly-hq <sam@donorly.org>
2026-07-03 05:49:13 +0530

    Refactor PledgeCardController to use new service methods

--- src/main/java/org/donorly/donorly_backend/controller/PledgeCardController.java
+++ src/main/java/org/donorly/donorly_backend/controller/PledgeCardController.java
@@ -1,50 +1,56 @@
 package org.donorly.donorly_backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.PledgeCard;
 import org.donorly.donorly_backend.service.PledgeCardService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/pledge-cards")
-@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class PledgeCardController {
 
-    @Autowired
-    private PledgeCardService pledgeCardService;
+    private final PledgeCardService pledgeCardService;
 
     @GetMapping
     public List<PledgeCard> getAll() {
-        return pledgeCardService.getAllPledgeCards();
-    }
-
-    @GetMapping("/unprocessed")
-    public List<PledgeCard> getUnprocessed() {
-        return pledgeCardService.getUnprocessedCards();
+        return pledgeCardService.getAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<PledgeCard> getById(@PathVariable String id) {
-        return pledgeCardService.getPledgeCardById(id)
+        return pledgeCardService.getById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/donor/{donorId}")
+    public List<PledgeCard> getByDonor(@PathVariable String donorId) {
+        return pledgeCardService.getByDonorId(donorId);
+    }
+
+    @GetMapping("/ambassador/{ambassadorId}")
+    public List<PledgeCard> getByAmbassador(@PathVariable String ambassadorId) {
+        return pledgeCardService.getByAmbassadorId(ambassadorId);
+    }
+
     @PostMapping
     public PledgeCard create(@RequestBody PledgeCard pledgeCard) {
-        return pledgeCardService.createPledgeCard(pledgeCard);
+        return pledgeCardService.save(pledgeCard);
     }
 
     @PutMapping("/{id}")
     public PledgeCard update(@PathVariable String id, @RequestBody PledgeCard pledgeCard) {
-        return pledgeCardService.updatePledgeCard(id, pledgeCard);
+        pledgeCard.setId(id);
+        return pledgeCardService.save(pledgeCard);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
-        pledgeCardService.deletePledgeCard(id);
-        return ResponseEntity.ok().build();
+        pledgeCardService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
