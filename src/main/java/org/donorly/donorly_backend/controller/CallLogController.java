1bf0b57c58cc7e161ee68b79e61414ff113f7109
donorly-hq <sam@donorly.org>
2026-07-03 05:50:45 +0530

    Refactor CallLogController to use new service methods

--- src/main/java/org/donorly/donorly_backend/controller/CallLogController.java
+++ src/main/java/org/donorly/donorly_backend/controller/CallLogController.java
@@ -1,50 +1,56 @@
 package org.donorly.donorly_backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.CallLog;
 import org.donorly.donorly_backend.service.CallLogService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/call-logs")
-@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class CallLogController {
 
-    @Autowired
-    private CallLogService callLogService;
+    private final CallLogService callLogService;
 
     @GetMapping
     public List<CallLog> getAll() {
-        return callLogService.getAllCallLogs();
+        return callLogService.getAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<CallLog> getById(@PathVariable String id) {
-        return callLogService.getCallLogById(id)
+        return callLogService.getById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/donor/{donorId}")
     public List<CallLog> getByDonor(@PathVariable String donorId) {
-        return callLogService.getCallLogsByDonor(donorId);
+        return callLogService.getByDonorId(donorId);
+    }
+
+    @GetMapping("/ambassador/{ambassadorId}")
+    public List<CallLog> getByAmbassador(@PathVariable String ambassadorId) {
+        return callLogService.getByAmbassadorId(ambassadorId);
     }
 
     @PostMapping
     public CallLog create(@RequestBody CallLog callLog) {
-        return callLogService.createCallLog(callLog);
+        return callLogService.save(callLog);
     }
 
     @PutMapping("/{id}")
     public CallLog update(@PathVariable String id, @RequestBody CallLog callLog) {
-        return callLogService.updateCallLog(id, callLog);
+        callLog.setId(id);
+        return callLogService.save(callLog);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
-        callLogService.deleteCallLog(id);
-        return ResponseEntity.ok().build();
+        callLogService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
