e0f272e54931f827e8be83aa31bb561669856270
donorly-hq <sam@donorly.org>
2026-07-03 05:38:00 +0530

    Change PledgeCardRepository to use JpaRepository

--- src/main/java/org/donorly/donorly_backend/repository/PledgeCardRepository.java
+++ src/main/java/org/donorly/donorly_backend/repository/PledgeCardRepository.java
@@ -1,12 +1,10 @@
 package org.donorly.donorly_backend.repository;
 
 import org.donorly.donorly_backend.model.PledgeCard;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface PledgeCardRepository extends MongoRepository<PledgeCard, String> {
-    List<PledgeCard> findByStatus(String status);
+public interface PledgeCardRepository extends JpaRepository<PledgeCard, String> {
     List<PledgeCard> findByDonorId(String donorId);
     List<PledgeCard> findByAmbassadorId(String ambassadorId);
-    List<PledgeCard> findByVolunteerId(String volunteerId);
 }
