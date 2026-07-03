b0e7c04008d8d2862fbe7c1ff55e4c4d36e49862
donorly-hq <sam@donorly.org>
2026-07-03 05:49:45 +0530

    Refactor TownHallController to use updated service methods

--- src/main/java/org/donorly/donorly_backend/controller/TownHallController.java
+++ src/main/java/org/donorly/donorly_backend/controller/TownHallController.java
@@ -1,50 +1,51 @@
 package org.donorly.donorly_backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.TownHall;
 import org.donorly.donorly_backend.service.TownHallService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/townhalls")
-@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class TownHallController {
 
-    @Autowired
-    private TownHallService townHallService;
+    private final TownHallService townHallService;
 
     @GetMapping
     public List<TownHall> getAll() {
-        return townHallService.getAllTownHalls();
+        return townHallService.getAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<TownHall> getById(@PathVariable String id) {
-        return townHallService.getTownHallById(id)
+        return townHallService.getById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
-    @GetMapping("/by-ambassador/{ambassadorId}")
+    @GetMapping("/ambassador/{ambassadorId}")
     public List<TownHall> getByAmbassador(@PathVariable String ambassadorId) {
-        return townHallService.getByHostAmbassador(ambassadorId);
+        return townHallService.getByAmbassador(ambassadorId);
     }
 
     @PostMapping
     public TownHall create(@RequestBody TownHall townHall) {
-        return townHallService.createTownHall(townHall);
+        return townHallService.save(townHall);
     }
 
     @PutMapping("/{id}")
     public TownHall update(@PathVariable String id, @RequestBody TownHall townHall) {
-        return townHallService.updateTownHall(id, townHall);
+        townHall.setId(id);
+        return townHallService.save(townHall);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
-        townHallService.deleteTownHall(id);
-        return ResponseEntity.ok().build();
+        townHallService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
