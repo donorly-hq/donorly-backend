aa6032e35a5977d1e6ebe1ba029a735cb8f09660
donorly-hq <sam@donorly.org>
2026-07-03 05:37:33 +0530

    Change DonorRepository to use JPA instead of MongoDB

--- src/main/java/org/donorly/donorly_backend/repository/DonorRepository.java
+++ src/main/java/org/donorly/donorly_backend/repository/DonorRepository.java
@@ -1,11 +1,9 @@
 package org.donorly.donorly_backend.repository;
 
 import org.donorly.donorly_backend.model.Donor;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface DonorRepository extends MongoRepository<Donor, String> {
+public interface DonorRepository extends JpaRepository<Donor, String> {
     List<Donor> findByAmbassadorId(String ambassadorId);
-    List<Donor> findByStatus(String status);
-    List<Donor> findByDonationType(String donationType);
 }
