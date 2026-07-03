6483f4c591046a2e67d247e31270bd5b0cf814b8
donorly-hq <sam@donorly.org>
2026-07-03 05:42:35 +0530

    Refactor PledgeCardService for cleaner code

--- src/main/java/org/donorly/donorly_backend/service/PledgeCardService.java
+++ src/main/java/org/donorly/donorly_backend/service/PledgeCardService.java
@@ -1,43 +1,40 @@
 package org.donorly.donorly_backend.service;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.PledgeCard;
 import org.donorly.donorly_backend.repository.PledgeCardRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.LocalDateTime;
+
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class PledgeCardService {
 
-    @Autowired
-    private PledgeCardRepository pledgeCardRepository;
+    private final PledgeCardRepository pledgeCardRepository;
 
-    public List<PledgeCard> getAllPledgeCards() {
+    public List<PledgeCard> getAll() {
         return pledgeCardRepository.findAll();
     }
 
-    public List<PledgeCard> getUnprocessedCards() {
-        return pledgeCardRepository.findByStatus("unprocessed");
-    }
-
-    public Optional<PledgeCard> getPledgeCardById(String id) {
+    public Optional<PledgeCard> getById(String id) {
         return pledgeCardRepository.findById(id);
     }
 
-    public PledgeCard createPledgeCard(PledgeCard pledgeCard) {
-        pledgeCard.setStatus("unprocessed");
-        pledgeCard.setCreatedAt(LocalDateTime.now());
+    public List<PledgeCard> getByDonorId(String donorId) {
+        return pledgeCardRepository.findByDonorId(donorId);
+    }
+
+    public List<PledgeCard> getByAmbassadorId(String ambassadorId) {
+        return pledgeCardRepository.findByAmbassadorId(ambassadorId);
+    }
+
+    public PledgeCard save(PledgeCard pledgeCard) {
         return pledgeCardRepository.save(pledgeCard);
     }
 
-    public PledgeCard updatePledgeCard(String id, PledgeCard updated) {
-        updated.setId(id);
-        return pledgeCardRepository.save(updated);
-    }
-
-    public void deletePledgeCard(String id) {
+    public void delete(String id) {
         pledgeCardRepository.deleteById(id);
     }
 }
