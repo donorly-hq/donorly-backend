601cc34364221e44bb954bb610983ed715931efb
donorly-hq <sam@donorly.org>
2026-07-03 05:48:39 +0530

    Refactor DonorController to use new service methods

--- src/main/java/org/donorly/donorly_backend/controller/DonorController.java
+++ src/main/java/org/donorly/donorly_backend/controller/DonorController.java
@@ -1,50 +1,51 @@
 package org.donorly.donorly_backend.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.donorly.donorly_backend.model.Donor;
 import org.donorly.donorly_backend.service.DonorService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/donors")
-@CrossOrigin(origins = "*")
+@RequiredArgsConstructor
 public class DonorController {
 
-    @Autowired
-    private DonorService donorService;
+    private final DonorService donorService;
 
     @GetMapping
     public List<Donor> getAll() {
-        return donorService.getAllDonors();
+        return donorService.getAll();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Donor> getById(@PathVariable String id) {
-        return donorService.getDonorById(id)
+        return donorService.getById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping("/ambassador/{ambassadorId}")
     public List<Donor> getByAmbassador(@PathVariable String ambassadorId) {
-        return donorService.getDonorsByAmbassador(ambassadorId);
+        return donorService.getByAmbassadorId(ambassadorId);
     }
 
     @PostMapping
     public Donor create(@RequestBody Donor donor) {
-        return donorService.createDonor(donor);
+        return donorService.save(donor);
     }
 
     @PutMapping("/{id}")
     public Donor update(@PathVariable String id, @RequestBody Donor donor) {
-        return donorService.updateDonor(id, donor);
+        donor.setId(id);
+        return donorService.save(donor);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id) {
-        donorService.deleteDonor(id);
-        return ResponseEntity.ok().build();
+        donorService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
