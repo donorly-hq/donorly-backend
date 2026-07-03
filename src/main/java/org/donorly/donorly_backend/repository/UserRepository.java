92ec0d88faba1a5638ed1d8c35f2a6fc77ca65de
donorly-hq <sam@donorly.org>
2026-07-03 05:36:40 +0530

    Refactor UserRepository to use JPA
    Changed UserRepository to extend JpaRepository instead of MongoRepository and removed unused methods.

--- src/main/java/org/donorly/donorly_backend/repository/UserRepository.java
+++ src/main/java/org/donorly/donorly_backend/repository/UserRepository.java
@@ -1,12 +1,9 @@
 package org.donorly.donorly_backend.repository;
 
 import org.donorly.donorly_backend.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByRole(String role);
-    Optional<User> findByEmailAndRole(String email, String role);
-    Optional<User> findByAmbassadorId(String ambassadorId);
 }
